80bb74f0dbe4bad50deff0ac8aa1126e2d97fae5
MarvinF <marviinflores@criptext.com>
2021-03-15 16:54:37 -0500

    SecondCommit - Card 1

---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Card() {
+    return (
+        <div className = "card">
+          <div className = "card-body"> 
+             <h4 className = "card-title">My Title</h4>
+             <p className = "card-text text-secondary">Lorem30 skd;lsk;lsa jdslskdd
+             jsajsadjsalkdjs sad;s;ksksoddu sdoisudioud d adas dosaudoias
+             dpsaidipodisap  jjsjsaldj dlsjdsadsa d dpoaidi idsidpoidpo
+             </p>
+          </div>
+        </div>
+    )
+}
+
+export default Card
